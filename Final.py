573c34f396693d6bce70a8c6c9bbcca8ca58b114
KiwiKajitsu <44657047+KiwiKajitsu@users.noreply.github.com>
2018-12-10 20:07:32 -0600

    Some documentation

--- Final.py
+++ Final.py
@@ -1,7 +1,14 @@
 
 
 '''
-
+#Authors: Alex Daigre, Jeff Butera, Flynn Keeler, Justin Henn, Paul Ries, Zachary Jorn
+#Date: 12/05/2018
+#Course Title: Intro to the Software Profession
+#v1.0
+#This program is compiled in Python 3.7
+#Purpose: This program is a game for the robot Cozmo to fine a cube that the player sets on a custom made board.
+#   The player creates a maxe on the game board using direction tiles, shice lead Cozmo to the cube.
+#   Cozmo goes though that maze by reading the direction tiles, once he finds the cube, he perfroms an animation.
 '''
 
 import time
